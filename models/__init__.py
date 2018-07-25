8dbd85fb4c1a21012180ea7aae30a4458a325d77
ktaebum <phya.ktaebum@gmail.com>
2018-07-25 14:58:51 +0900

    modify __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
-from models.googlenet import GoogleNet
+from models.googlenet import GoogleNet, GoogleNetTrainer
+from models.resnet import ResNet, ResNetTrainer
 
 __all__ = [attr for attr in dir() if not attr.startswith('_')]
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from utils.estimates import estimates_function_runtime
 from utils.model_io import load_parallel_state_dict
+from utils.train import ModelTrainer
